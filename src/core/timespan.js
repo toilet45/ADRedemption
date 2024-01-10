98d8f75a4508006a94105b141cd6795e0c6590bd
Sxy6214 <sxy6214@foxmail.com>
2024-01-10 17:13:00 +0800

    Added ms display for timespan
    in order to make it showing in EC12

--- src/core/timespan.js
+++ src/core/timespan.js
@@ -223,6 +223,7 @@ window.TimeSpan = class TimeSpan {
     addCheckedComponent(this.hours, "hour");
     addCheckedComponent(this.minutes, "minute");
     addCheckedComponent(this.seconds, "second");
+    addCheckedComponent(this.milliseconds, "millisecond");
     // Join with commas and 'and' in the end.
     if (parts.length == 0) return `${formatInt(0)} seconds`;
     return [parts.slice(0, -1).join(", "), parts.slice(-1)[0]].join(parts.length < 2 ? "" : " and ");
