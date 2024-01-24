9886a7d3b2debe6c45a311c36b0e607d1239eedc
Sxy6214 <sxy6214@foxmail.com>
2024-01-24 20:58:43 +0800

    ~

---
+++ src/core/secret-formula/celestials/kohler-unlock-progress.js
@@ -0,0 +1,16 @@
+export const kohler = {
+  progressUnlocks: {
+    kohlerUnlocked: {
+      id: 0,
+      description: "Kohler tab is unlocked, yet too early.",
+    },
+    hostileScore: {
+      id: 1,
+      description: "Reach idk Hostile score: idk adward.",
+    },
+    placeholder: {
+      id: 2,
+      description: "A placeholder that cannot be unlocked to see style.",
+    }
+  }
+};
