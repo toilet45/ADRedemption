7153f09ef87bb4438a29ce72347518cf2a901b8e
Sxy6214 <sxy6214@foxmail.com>
2024-01-29 11:28:44 +0800

    nerfing

--- src/core/secret-formula/mending/corruption-upgrades.js
+++ src/core/secret-formula/mending/corruption-upgrades.js
@@ -147,7 +147,7 @@ export const corruptionUpgrades = [
     lockEvent: "Illegal lock called - Please report this with your save and what you did.",
     description: "Black Hole 1 & 2's base multiplier is increased.",
     effect: () => 2.5,
-    formatEffect: value => formatX(value, 0, 0)
+    formatEffect: value => formatX(value, 1, 1)
   },
   {
     name: "Nerf Every Curse",
