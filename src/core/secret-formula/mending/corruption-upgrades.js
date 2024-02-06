2af4c993ccbb7633faf2c237a1c91bcb7debb78c
Sxy6214 <sxy6214@foxmail.com>
2024-02-06 15:04:52 +0800

    ~

--- src/core/secret-formula/mending/corruption-upgrades.js
+++ src/core/secret-formula/mending/corruption-upgrades.js
@@ -267,7 +267,7 @@ export const corruptionUpgrades = [
     canLock: false,
     lockEvent: "Illegal lock called - Please report this with your save and what you did.",
     description: "If Complex Glyphs is level 4 or higher, gain a power effect to score, based on glyph levels and Complex Glyphs level.",
-    effect: () => player.mending.corruptionChallenge.corruptedMend && (player.mending.corruption[4] >= 4) ? (1 + Math.log(Math.log(player.mending.corruption[4] * Math.max(1,player.records.bestReality.glyphLevel)))/2.5)**0.9 : 1,
+    effect: () => player.mending.corruptionChallenge.corruptedMend && (player.mending.corruption[4] >= 4) ? (1 + Math.log(Math.log(player.mending.corruption[4] * Math.max(1,player.records.bestReality.glyphLevel)))/2.5) : 1,
     formatEffect: value => formatPow(value, 2, 2)
   },
   {
